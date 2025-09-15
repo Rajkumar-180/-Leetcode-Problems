134c38c436852c8e392bc9c60e005338108aeaca
Rajkumar Periyasamy <rajkumarperiyasamy2005@gmail.com>
2025-09-15 10:02:49 +0530

    Time: 3 ms (48.72%) | Memory: 42.7 MB (33.87%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,22 +1,22 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-        String arr[] = text.split(" ");
-        int a = arr.length;
-        boolean b = false;
-        int count = 0;
+        String arr[] = text.split(" "); //Convert the one string to string array
+        int a = arr.length; //length of the array
+        boolean b = false; 
+        int count = 0; //Declare Non Matched words count = 0
         for(int i=0;i<a;i++)
         {
             b= true;
-            for(int j=0;j<arr[i].length();j++)
+            for(int j=0;j<arr[i].length();j++) //give the index of i.length to condition
             {
-               char c = arr[i].charAt(j);
-               if (brokenLetters.contains(String.valueOf(c)))
+               char c = arr[i].charAt(j); 
+               if (brokenLetters.contains(String.valueOf(c))) //check the character as been contained or not
                {
                 b=false;
                 break;
                }
             }
-        if(b)
+        if(b)//if true
         {
              count++;
         }
