1a46bc9c20023ea7dd506d78a4fd4ddfda383f4d
Rajkumar Periyasamy <rajkumarperiyasamy2005@gmail.com>
2026-06-07 20:58:29 +0530

    Add: 2306. Create Binary Tree From Descriptions (Java)

---
+++ Array/Medium/2306-create-binary-tree-from-descriptions.java
@@ -0,0 +1,69 @@
+// ═══════════════════════════════════════════════════════
+// Problem: 2306. Create Binary Tree From Descriptions
+// Difficulty: Medium
+// Topics: Array, Hash Table, Tree, Binary Tree
+// Runtime: 78 ms (Beats 15.4%)
+// Memory: 87 MB (Beats 31.1%)
+// Submitted: Jun 7, 2026
+// Link: https://leetcode.com/problems/create-binary-tree-from-descriptions/
+// ═══════════════════════════════════════════════════════
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer, TreeNode> mp = new HashMap<>();
+        HashMap<Integer, Integer> root = new HashMap<>();
+
+        for (int[] d : descriptions) {
+
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            if (!mp.containsKey(parent)) {
+                mp.put(parent, new TreeNode(parent));
+            }
+
+            if (!mp.containsKey(child)) {
+                mp.put(child, new TreeNode(child));
+            }
+
+            if (isLeft == 1) {
+                mp.get(parent).left = mp.get(child);
+            } else {
+                mp.get(parent).right = mp.get(child);
+            }
+
+            if (root.getOrDefault(parent, 0) != -1) {
+                root.put(parent, 1);
+            }
+
+            root.put(child, -1);
+        }
+
+        int rootVal = 0;
+
+        for (var entry : root.entrySet()) {
+            if (entry.getValue() == 1) {
+                rootVal = entry.getKey();
+                break;
+            }
+        }
+
+        return mp.get(rootVal);
+    }
+}
